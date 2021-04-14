d944629aea84519dedb103ea39d85dc49f0d2561
Ravi <raviteja.valluri@rackspace.com>
2021-04-14 11:40:08 -0700

    HTML for Search, Header and Footer

--- api-docs/conf.py
+++ api-docs/conf.py
@@ -184,10 +184,10 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'Rackspace Cloud Images v2.0 documentation'
+# html_title = 'Rackspace Cloud Images v2.0 documentation'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = 'Rackspace Cloud Images v2.0 API'
+# html_short_title = 'Rackspace Cloud Images v2.0 API'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
